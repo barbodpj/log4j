eb6bbed1442cd820f34ea900e0c73b84e25f0b7b
barbodpj <you@example.com>
2018-09-16 12:08:31 +0430

    sample program

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+public class Main {
+    private static Logger logger = Logger.getLogger("javaLogger");
+    public static void main(String[] args)  {
+        logger.info("sample info message");
+    }
+}
